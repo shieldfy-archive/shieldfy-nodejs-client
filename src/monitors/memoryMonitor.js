43d7eabcff45dcb5a0e164f6f1b243ba529350c9
Eslam Salem <eslam@shieldfy.com>
2019-03-11 00:10:39 +0300

    add param to reduce false positives

--- src/monitors/memoryMonitor.js
+++ src/monitors/memoryMonitor.js
@@ -13,18 +13,31 @@ memoryMonitor.prototype.run = function(Client)
     Shimmer.wrap(Buffer, 'from', function (original) {
         return function (buff) {
             if (Client._currentRequest) {
-                // TODO: test and remove try
-                // add try beacuse this function is used before the rules get downloaded
                 try{
-                    let Judge = Client._jury.use('memory');
-                    let result = Judge.execute(buff);
-                    if(result){
-                        Client._currentRequest._score += result.score;
-                        Client.sendToJail();
-                        var stack = new Error().stack;
-                        new StackCollector(stack).parse(function(codeInfo){
-                            Client.reportThreat('memory', result, codeInfo);
-                        });
+                    let value = buff;
+                    let requestParams = Client._currentRequest.getParam();
+                   
+                    if(!(Object.keys(requestParams).length === 0 && requestParams.constructor === Object)){
+                        
+                        for(let param in requestParams){
+                           
+                            let paramValue = requestParams[param];
+                            
+                            if(value.indexOf(paramValue) !== -1){
+                                console.log('Judge');
+                                let Judge = Client._jury.use('memory');
+                                let result = Judge.execute(paramValue);
+                                if(result){
+                                    console.log('Guilty');
+                                    Client._currentRequest._score += result.score;
+                                    Client.sendToJail();
+                                    var stack = new Error().stack;
+                                    new StackCollector(stack).parse(function(codeInfo){
+                                        Client.reportThreat('memory', result, codeInfo);
+                                    });
+                                }
+                            }
+                        }
                     }
                 }catch(e){}
             }
