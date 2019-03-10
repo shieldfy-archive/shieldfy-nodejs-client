32d3e4473ad47453508936203403b4a8507543ee
Eslam Salem <eslam@shieldfy.com>
2019-03-11 00:12:04 +0300

    fix response limit and content encoding

--- src/session.js
+++ src/session.js
@@ -16,36 +16,12 @@ const Session = function(Client){
             return function (event, req, res) {        
                 if (event === 'request') {
                     Client._currentRequest = new Request()
-
                     Client._currentRequest.start(req,res);
-
 
                     res.on('finish',function() {
                         try{
                             Client._currentRequest.setRes(res);
-                        }catch(e) {}
-                        
-                        /**
-                         * Disable session step for now to optimize the execution
-                         */
-                        // Client._http._api.trigger('session/step', {
-                        //     sessionId: Client._sessionId,
-                        //     host: Client._currentRequest._headers.host,
-                        //     info: {
-                        //         method: Client._currentRequest._method,
-                        //         uri: Client._currentRequest._url.uri,
-                        //         code: Client._currentRequest._statusCode,
-                        //         time: Client._currentRequest._created
-                        //     },
-                        //     user: {
-                        //         id: Client._currentRequest._id,
-                        //         ip: Client._currentRequest._ip,
-                        //         userAgent: Client._currentRequest._headers['user-agent'],
-                        //         score: Client._currentRequest._score,
-                        //     },  
-                        // });
-    
-        
+                        }catch(e) {}   
                     });
                 }      
                 var returned = original.apply(this, arguments)
@@ -53,33 +29,37 @@ const Session = function(Client){
             };
         });
 
+        Shimmer.wrap(exports && exports.ServerResponse && exports.ServerResponse.prototype, 'setHeader', function (original) {
+            return function (key,value) {
+                if( Client._currentRequest.isDanger() &&  key == 'Content-Length'){
+                    arguments[1] = String(Client._response.block().length); 
+                }
+                
+                var returned = original.apply(this, arguments);
+                return returned;
+            };
+        });
+
         Shimmer.wrap(exports && exports.ServerResponse && exports.ServerResponse.prototype, 'writeHead', function (original) {
             return function () {
                 try{
-
                     if(Client._currentRequest.isDanger()){
-                        arguments[0] = '403';
+                        arguments[0] = '200';
                         arguments[1] = {
                             'Content-Type': 'text/html',
-                            'Transfer-Encoding': 'chunked'
+                            'Transfer-Encoding': 'identity'
                         };
                     }
                 }catch(e){}
-    
-    
                 var returned = original.apply(this, arguments);
                 return returned;
             };
         });
 
         Shimmer.wrap(exports && exports.ServerResponse && exports.ServerResponse.prototype, 'write', function (original) {
-            return function () {
+            return function (data) {
                 try {
                     if(Client._currentRequest.isDanger()){
-                        try {
-                            Client._currentRequest._$res.setHeader('Transfer-Encoding','chunked');
-                            Client._currentRequest._$res.setHeader('Content-Type','text/html');
-                        }catch (e) {}
                         arguments[0] = '';
                     }
     
@@ -94,10 +74,10 @@ const Session = function(Client){
             return function () {
                 try {
                     if(Client._currentRequest.isDanger()){
-                        try {
-                            Client._currentRequest._$res.setHeader('Transfer-Encoding','chunked');
-                            Client._currentRequest._$res.setHeader('Content-Type','text/html');
-                        }catch (e) {}
+                        // try {
+                        //     // Client._currentRequest._$res.setHeader('Transfer-Encoding','identity');
+                        //     Client._currentRequest._$res.setHeader('Content-Type','text/html');
+                        // }catch (e) {}
                         arguments[0] = Client._response.block();
                     }
                     Client._currentRequest.end();
